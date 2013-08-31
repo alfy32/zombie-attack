3fe6a6cca5274c9a2a4f6baf824980143b61aa29
Gregor <gdaniels13@gmail.com>
2013-08-31 09:46:44 -0600

    rudimentary authentication working

--- zombieAttack/app.js
+++ zombieAttack/app.js
@@ -8,14 +8,17 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
+var fs = require('fs');
 var app = express();
+var cradle = require('cradle');
+var bcrypt = require('bcrypt');
+
+var adduser = require('./usefulscripts/adduser');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -30,8 +33,71 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//setup the users database
+
+var users = new(cradle.Connection)().database('users');
+
+//app.get('/', routes.index);
+// app.get('/users', user.list);
+
+app.post('/', function(req,res)
+{
+	var id = req.body.email;
+	var pass = req.body.password;
+	verify(id,pass,res);
+
+});
+app.post('/createUser',function(req,res){
+	var user = new Object();
+	user['name'] = req.body.name;
+	user['password']= req.body.password;
+	user['email']=req.body.email
+	adduser.new(bcrypt,users,user);
+	res.send('<h1>success<h1>');
+});
+
+
+function verify(id,pass,res)
+{
+	console.log(id);
+	console.log(pass);
+	users.get(id, function(err,doc){
+		if(err)
+		{
+			console.log("failed to get user from database");
+			res.redirect('/');
+		}
+		else{
+			var newhash=doc.password;
+			console.log(doc);
+			bcrypt.compare(pass,newhash,function(err,result){
+				if(err){
+					console.log('failed to compare');
+					res.redirect('/');
+				}
+
+				if(result){
+					res.redirect('/loginSuccess.html');
+				}
+				else{
+					res.redirect('/');
+				}
+			});
+
+
+
+			// if(newpass==pass)
+			// {
+			// 	console.log('success');
+			// 	res.redirect('/loginSuccess.html');
+			// }
+			// else
+			// {
+			// 	res.redirect('/');
+			// }
+		}
+	});
+}
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
