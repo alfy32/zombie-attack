84c48be7da2eddc270f6d75771585a4a3435bf95
alfy32 <alfy32@gmail.com>
2013-09-03 19:12:26 -0600

    added gregs changes to my branch

--- zombieAttack/usefulscripts/adduser.js
+++ zombieAttack/usefulscripts/adduser.js
@@ -1,15 +1,12 @@
 
 exports.new = function(bcrypt,usersdb,user){
-	bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(user.password, salt, function(err, hash) {
+	   bcrypt.hash(user.password,null,null , function(err, hash) {
     	usersdb.save(user.email,{name:user.name,password:hash},function(err,res){
     		if(err)
     			console.log('failed to create user' + user.email);
     		else
     			console.log('successfully created user' + user.email);
-
     	});
 
     });
-});
 };
