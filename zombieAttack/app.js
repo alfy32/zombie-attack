88af678ae3c877c3301ad0adc6a07084d3c87f5b
Alan <alan@alan-VirtualBox.(none)>
2013-09-04 21:57:32 -0600

    changed the database to cloudant. Much faster!!

--- zombieAttack/app.js
+++ zombieAttack/app.js
@@ -38,12 +38,12 @@ if ('development' == app.get('env')) {
 //setup the users database
 
 //uncoment the next three lines if you want to use a remote database
-// var connection = new(cradle.Connection)('http://gdaniels13.iriscouch.com',5984,
-// 	{auth:{username:'zombie',password:'eatbrains'}});
-// var users = connection.database('users');
+ var connection = new(cradle.Connection)('http://zombie.cloudant.com',5984,
+ 	{auth:{username:'zombie',password:'eatbrains'}});
+ var users = connection.database('users');
 
 //uncoment this line if you want to use the local database
-var users = new(cradle.Connection)().database('users');
+//var users = new(cradle.Connection)().database('users');
 
 
 app.post('/', function(req,res)
@@ -101,7 +101,6 @@ app.post('/createUser',checkAuth, function(req,res){
 	adduser.new(bcrypt,users,user);
 	res.send('<h1>success<h1>');
 });
-
 
 function checkAuth(req, res, next) {
 	var lastActivity = new Date().getTime() - req.session.lastActivity;
