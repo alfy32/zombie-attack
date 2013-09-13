1717024cbab46d8f464d017ddaea0c90453a4d7d
alfy32 <alfy32@gmail.com>
2013-09-12 22:21:48 -0600

    Changing to new object

--- zombieAttack/public/javascripts/Map.js
+++ zombieAttack/public/javascripts/Map.js
@@ -1,27 +1,103 @@
 
-function Map(id) {
-   this.map = document.getElementById(id);
-   this.context = this.map.getContext("2d");
-   this.boxSize = 25;
-   this.setBoxSize = function (size) { 
-      this.boxSize = size;
-   };
-   this.getBoxSize = function () {
-      return this.boxSize;
-   };
-};
+function Map() {
+    var _canvas = document.getElementById('map');
+    var _context = _canvas.getContext('2d');
+    var _tileSize = 40;
 
-Map.prototype.getMap = function () {
-   return this.map;
-};
+    var _leftClickTile = 0;
+    var _rightClickTile = 1;
 
-Map.prototype.getContext = function () {
-   return this.context;
-};
+    var _mouseButtonClicked = false;
+    var _mouseX = 0;
+    var _mouseY = 0;
 
+    var _tiles = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ];
 
+    disableRightClick();
 
-Map.prototype.drawRectangle = function (x,y,color) {
-   this.context.fillStyle = color;
-   this.context.fillRect(x, y, this.boxSize, this.boxSize);
+    this.getTile = function(row, col) {
+        return _tiles[row][col];
+    };
+
+    this.setTile = function(row, col, tileNumber) {
+        _tiles[row][col] = tileNumber;
+    };
+
+    this.getTileSize = function() {
+        return _tileSize;
+    };
+
+    this.drawRectangle = function(x, y, color) {
+        _context.fillStyle = color;
+        _context.fillRect(x, y, _tileSize, _tileSize);
+    };
+
+    function disableRightClick() {
+        $(_canvas).bind("contextmenu", function(e) {
+            e.preventDefault();
+        });
+    }
+
+    function currentBox() {
+        return [Math.floor(_mouseX / _tileSize), Math.floor(_mouseY / _tileSize)];
+    }
+
+    function currentBoxX() {
+        return Math.floor(_mouseX / _tileSize);
+    }
+    function currentBoxY() {
+        return Math.floor(_mouseY / _tileSize);
+    }
+
+    function bindMouseDown() {
+        $(_canvas).mousedown(function(event) {
+            _mouseButtonClicked = true;
+            _mouseX = event.offsetX;
+            _mouseY = event.offsetY;
+
+            if (event.which === 1) {
+                _tiles[currentBoxX(), currentBoxY()] = _leftClickTile;
+            } else if (event.which === 3) {
+                _tiles[currentBoxX(), currentBoxY()] = _rightClickTile;
+            }
+
+            writeMouseInfo(event);
+        });
+    }
+
+    function writeMouseInfo(event) {
+        $('#position').html("Button: " + event.which === 1 ? "Left " : "Right " + "X: " + x + " Y: " + y + "<br/>Clicked: " + _mouseButtonClicked);
+    }
 }
+
+function Tile() {
+    var _id = 0;
+    var _tileImage;
+
+    this.getId = function() {
+        return _id;
+    };
+
+    this.setId = function(tileId) {
+        _id = tileId;
+    };
+
+    this.getTileImage = function() {
+        return _tileImage;
+    };
+
+    this.setTileImage = function(tileImage) {
+        _tileImage = tileImage;
+    };
+}
+
+
--- zombieAttack/public/javascripts/mapBuilder.js
+++ zombieAttack/public/javascripts/mapBuilder.js
@@ -5,99 +5,99 @@ var boxSize = 25;
 
 var clicked = false;
 
-var map = new Map('map');
+var map = new Map();
 
 $(document).ready(function() {
-	$('#blockSize').val(map.getBoxSize());
+    $('#blockSize').val(map.getTileSize());
 
-	$('#blockSize').change(function(event) {
-		boxSize = $(this).val();
-	});
-	$('.col-sm-1').mousedown(function(event) {
-		event.preventDefault();
-		if (event.which === 1) {
-			leftColor = $(this).css("background-color");
-			$('#leftClick').css("background-color", leftColor);
-		} else if (event.which === 3) {
-			rightColor = $(this).css("background-color");
-			$('#rightClick').css("background-color", rightColor);
-		}
+    $('#blockSize').change(function(event) {
+        boxSize = $(this).val();
+    });
+    $('.col-sm-1').mousedown(function(event) {
+        event.preventDefault();
+        if (event.which === 1) {
+            leftColor = $(this).css("background-color");
+            $('#leftClick').css("background-color", leftColor);
+        } else if (event.which === 3) {
+            rightColor = $(this).css("background-color");
+            $('#rightClick').css("background-color", rightColor);
+        }
 
-	});
+    });
 
-	$('.col-sm-1').bind("contextmenu", function(e) {
-		e.preventDefault();
-	});
+    $('.col-sm-1').bind("contextmenu", function(e) {
+        e.preventDefault();
+    });
 
-	$('#map').bind("contextmenu", function(e) {
-		e.preventDefault();
-	});
+    $('#map').bind("contextmenu", function(e) {
+        e.preventDefault();
+    });
 
-	$('#map').mousedown(function(event) {
-		clicked = true;
-		var html;
-		var x = Math.floor(event.offsetX / boxSize) * boxSize;
-		var y = Math.floor(event.offsetY / boxSize) * boxSize;
-
-		if (event.which === 1) {
-			html = "Button: Left<br/>";
-
-			drawRectangle('map', x, y, leftColor);
-
-		} else if (event.which === 3) {
-			html = "Button: Right<br/>";
-
-			drawRectangle('map', x, y, rightColor);
-		}
-		$('#position').html(html + "X: " + x + " Y: " + y + "<br/>Clicked: " + clicked);
-	});
-
-	$('#map').mousemove(function(event) {
-		if (clicked === true) {
-
-			var html;
-			var x = Math.floor(event.offsetX / boxSize) * boxSize;
-			var y = Math.floor(event.offsetY / boxSize) * boxSize;
-
-			if (event.which === 1) {
-				html = "Button: Left<br/>";
-
-				drawRectangle('map', x, y, leftColor);
-
-			} else if (event.which === 3) {
-				html = "Button: Right<br/>";
-
-				drawRectangle('map', x, y, rightColor);
-			}
-			$('#position').html(html + "X: " + x + " Y: " + y + "<br/>Clicked: " + clicked);
-		}
-	});
-
-	$('#map').mouseup(function(event) {
-		clicked = false;
-		var html = 'Mouse Up<br/>';
-		var x = Math.floor(event.offsetX / boxSize) * boxSize;
-		var y = Math.floor(event.offsetY / boxSize) * boxSize;
-		$('#position').html(html + "X: " + x + " Y: " + y + "<br/>Clicked: " + clicked);
-	});
+//    $('#map').mousedown(function(event) {
+//        clicked = true;
+//        var html;
+//        var x = Math.floor(event.offsetX / boxSize) * boxSize;
+//        var y = Math.floor(event.offsetY / boxSize) * boxSize;
+//
+//        if (event.which === 1) {
+//            html = "Button: Left<br/>";
+//
+//            map.drawRectangle(x, y, leftColor);
+//
+//        } else if (event.which === 3) {
+//            html = "Button: Right<br/>";
+//
+//            drawRectangle('map', x, y, rightColor);
+//        }
+//        $('#position').html(html + "X: " + x + " Y: " + y + "<br/>Clicked: " + clicked);
+//    });
+//
+//    $('#map').mousemove(function(event) {
+//        if (clicked === true) {
+//
+//            var html;
+//            var x = Math.floor(event.offsetX / boxSize) * boxSize;
+//            var y = Math.floor(event.offsetY / boxSize) * boxSize;
+//
+//            if (event.which === 1) {
+//                html = "Button: Left<br/>";
+//
+//                drawRectangle('map', x, y, leftColor);
+//
+//            } else if (event.which === 3) {
+//                html = "Button: Right<br/>";
+//
+//                drawRectangle('map', x, y, rightColor);
+//            }
+//            $('#position').html(html + "X: " + x + " Y: " + y + "<br/>Clicked: " + clicked);
+//        }
+//    });
+//
+//    $('#map').mouseup(function(event) {
+//        clicked = false;
+//        var html = 'Mouse Up<br/>';
+//        var x = Math.floor(event.offsetX / boxSize) * boxSize;
+//        var y = Math.floor(event.offsetY / boxSize) * boxSize;
+//        $('#position').html(html + "X: " + x + " Y: " + y + "<br/>Clicked: " + clicked);
+//    });
 
 });
 
 function drawRectangle(id, x, y, color) {
 
-	var c = document.getElementById(id);
-	var ctx = c.getContext("2d");
+    var c = document.getElementById(id);
+    var ctx = c.getContext("2d");
 
-	ctx.fillStyle = color;
-	ctx.fillRect(x, y, boxSize, boxSize);
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, boxSize, boxSize);
 }
 
 function drawGrid() {
-	var c = document.getElementById("map");
-	var ctx = c.getContext("2d");
+    var c = document.getElementById("map");
+    var ctx = c.getContext("2d");
 
-	ctx.beginPath();
-	ctx.moveTo(25, 0);
-	ctx.lineTo(25, 600);
-	ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(25, 0);
+    ctx.lineTo(25, 600);
+    ctx.stroke();
 }
