162990526924cd7b5316f2f219736660c6f2c304
Tyson Lindhardt <hobartn@gmail.com>
2013-10-18 14:41:44 -0600

    ...

--- zombieAttack/app.js
+++ zombieAttack/app.js
@@ -170,7 +170,7 @@ app.get('/mapsrequest', checkAuth, function(req, res){
 			console.log('error');
 		else
 		{
-			//console.log(response);
+			console.log("successfully retrievd maps");
 			res.json(response);
 		}
 	});
