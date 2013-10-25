14ea3916e339888f0579c1ea918f6a8a2b1fe906
Tyson Lindhardt <hobartn@gmail.com>
2013-10-25 16:50:09 -0600

    sweet stuff

--- zombieAttack/public/javascripts/main.js
+++ zombieAttack/public/javascripts/main.js
@@ -42,3 +42,29 @@ function loadStartup()
 		$('#load-stuff-here').load('startup.html');
 	}
 }
+
+function playMap()
+{
+	var mapID = document.getElementsByClassName('list-group-item active');
+	$(".modal-title").html($(mapID).html());
+	console.log(mapID);
+	var request = {
+		mapid : $(mapID).attr('mapid')
+	};
+	$.post("/playMap", request, function(info)
+	{
+		console.log(info);
+		$(".play-area").attr('src',info.url);
+	});
+	console.log("play");
+}
+
+function editMap()
+{
+	console.log("edit");
+}
+
+function deleteMap()
+{
+	console.log("delete");
+}
